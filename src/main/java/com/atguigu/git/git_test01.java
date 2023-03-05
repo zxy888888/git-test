4f876bd342ee5a7e76a91e16e01bf5cf6aeb4844
Layne <Layne@atguigu.com>
2023-03-05 17:57:11 +0800

    third commit

--- src/main/java/com/atguigu/git/git_test01.java
+++ src/main/java/com/atguigu/git/git_test01.java
@@ -2,6 +2,8 @@ package com.atguigu.git;
 
 public class git_test01 {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
